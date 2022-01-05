bdc3e01c16f916c75fa0ff13392e8ff9d74ebef8
Md. Anisur Rahman <mrahman191191@bscse.uiu.ac.bd>
2022-01-05 23:31:53 +0600

    I have the whole code

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,10 +1,16 @@
 public class TestClass {
     int id;
     String name;
-    
+
     TestClass(int id, String name){
         this.id = id;
         this.name = name;
     }
 
+    public static void main(String[] args) {
+        TestClass tc = new TestClass(11, "Anisur");
+        
+    }
+
 }
+
